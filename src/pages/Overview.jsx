34b2bf7eaacb689a46a9b4bb379a04c38ef26a31
ridoy-mahmud <ridoymahmud678@gmail.com>
2025-05-14 15:24:41 +0600

    some frontend updates

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,17 +1,27 @@
-import AccountBalance from "../components/AccountBalance"
-import Statistics from "../components/Statistics"
-import TransactionsTable from "../components/TransactionsTable"
-import QuickTransfer from "../components/QuickTransfer"
+import AccountBalance from "../components/AccountBalance";
+import Statistics from "../components/Statistics";
+import TransactionsTable from "../components/TransactionsTable";
+import QuickTransfer from "../components/QuickTransfer";
 
 const Overview = ({ accounts, transactions, user }) => {
   return (
     <>
-      <h1 className="text-3xl font-semibold mb-8">Welcome, {user ? user.firstName : "Guest"}!</h1>
+      <h1 className="text-3xl font-semibold mb-8">
+        Welcome, {user ? user.firstName : "Guest"}!
+      </h1>
 
       <h2 className="text-2xl font-medium mb-6">Account Balance</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-8">
-        <AccountBalance title="Current Account" amount={accounts.current} currency="GHC" />
-        <AccountBalance title="Saving Account" amount={accounts.savings} currency="GHC" />
+        <AccountBalance
+          title="Current Account"
+          amount={accounts.current}
+          currency="BDT"
+        />
+        <AccountBalance
+          title="Saving Account"
+          amount={accounts.savings}
+          currency="BDT"
+        />
         <Statistics />
       </div>
 
@@ -24,7 +34,7 @@ const Overview = ({ accounts, transactions, user }) => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Overview
+export default Overview;
